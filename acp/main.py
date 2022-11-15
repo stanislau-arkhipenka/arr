700b5ffb8f32858db22d471f46a7228cddbd019e
Stanislau Arkhipenka <stanislau.arkhipenka@gmail.com>
2022-11-15 14:25:38 +0000

    Add default value for log level

--- acp/main.py
+++ acp/main.py
@@ -8,7 +8,7 @@ from acp.acp_robot import AcpRobot
 @click.option("--controller", type=str)
 @click.option("--debug-servo", is_flag=True, default=False)
 @click.option("--debug-led", is_flag=True, default=False)
-@click.option("--log-level", type=str)
+@click.option("--log-level", type=str, default="INFO")
 def main(config_file_path, controller=None, debug_servo=False, debug_led=False, log_level="INFO"):
     level = logging.getLevelName(log_level)
     logging.basicConfig(
